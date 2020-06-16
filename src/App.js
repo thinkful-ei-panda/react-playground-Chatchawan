6259559ead8c803946dbac3cf04678be4e720ba4
jenthura <joshuasth@gmail.com>
2020-06-16 11:14:54 -0500

    finished the jest instructions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Messages from './Messages';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    );
+  }
 }
 
 export default App;
